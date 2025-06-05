b0aabbec930bc321ff8bf3695c13490b7ccf5f17
laercioGmaia <maialgustavo@gmail.com>
2025-06-05 17:49:35 -0300

    Cadastro de usuários

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,11 +1,21 @@
 <template>
-    <h1>header</h1>
+    <header class="flex justify-between bg-blue-500 h-25 items-center px-5">
+        <h1 class="text-white text-2xl font-bold">Gerenciamento de tarefas</h1>
+        <nav class="flex gap-4 text-white">
+            <Link href="#">Cadastro de Usuario</Link>
+            <Link href="#">Cadastro de Tarefas</Link>
+            <Link href="#">Gerenciar Tarefas</Link>
+        </nav>
+    </header>
+    <main class="w-11/12 mx-auto">
+        <slot>
+
+        </slot>
+    </main>
 </template>
-    
+
 <script setup lang='ts'>
-    
+import { Link } from '@inertiajs/vue3'
 </script>
-    
-<style>
-    
-</style>
+
+<style></style>
---
+++ resources/js/pages/Register.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <Head title="Cadastro de Usuário" />
+    <AppLayout>
+        <h1 class="text-2xl font-bold">Cadastro de Usuário</h1>
+        <form class="flex flex-col gap-3 w-50" @submit.prevent="save">
+            <div class="flex flex-col">
+                <label for="name">Nome:</label>
+                <input class="border-2 rounded p-1" type="text" id="name" v-model="form.name">
+                <span class="text-red-500"> {{ form.errors.name }}</span>
+            </div class="flex flex-col">
+            <div>
+                <label for="email">Email</label>
+                <input class="border-2 rounded p-1" type="email" id="email" v-model="form.email">
+                <span class="text-red-500"> {{ form.errors.email }}</span>
+            </div>
+            <button type="submit" class="bg-blue-500 text-white p-2 rounded w-fit">Cadastrar</button>
+        </form>
+    </AppLayout>
+</template>
+
+<script setup lang='ts'>
+import AppLayout from "@/layouts/AppLayout.vue"
+import { Head, useForm } from '@inertiajs/vue3'
+
+const form = useForm({
+    name: '',
+    email: ''
+});
+
+const save = () => {
+    form.post(route('register.store'), {
+        onSuccess() {
+            alert('cadastrado com sucesso');
+        }
+    });
+}
+</script>
